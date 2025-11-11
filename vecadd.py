517f46c9842640c56c31e6f4010839cf91829654
2024ucp1667-droid <2024ucp1667@mnit.ac.in>
2025-11-11 15:36:58 +0530

    Update vecadd.py

--- vecadd.py
+++ vecadd.py
@@ -1,4 +1,4 @@
 
 def dot_product(a, b):
-    print("Dot product function - merged version (includes both improvements)")
+    print("Dot product function - merged version ")
     return sum(x * y for x, y in zip(a, b))
